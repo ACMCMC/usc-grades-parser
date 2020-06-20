4828467511b73d2257918ffbd65a86d3fcd849d9
Aldan Creo <20495460+ACMCMC@users.noreply.github.com>
2020-06-20 04:06:15 +0200

    Primera versión de release

--- parser.py
+++ parser.py
@@ -13,32 +13,35 @@ if nombre_arch == "":
 with open(nombre_arch if ".pdf" in nombre_arch else nombre_arch + ".pdf", "rb") as f:
     pdf = "\n".join(pdftotext.PDF(f))
 
-#with open("output.txt",'w') as out:
+# with open("output.txt",'w') as out:
 #    out.write(pdf)
 
 pdf = pdf.split("Observacións")
 pdf.pop(0)
-pdf = "".join(pdf) #Borramos el encabezado
+pdf = "".join(pdf)  # Borramos el encabezado
 notas = re.findall("(?<=\()\d+\.\d+(?=\))", pdf)
 nombres = re.findall("(?<=\,\s)((?:\w+\s)*\w+)(?=\s)", pdf)
 apellidos = re.findall("(?<=\s)((?:\d+\s)*\w+)(?=\,)", pdf)
 notas = [float(x) for x in notas]
 
-#Encontramos las líneas de los no presentados, porque se escapan al regex de las notas en números
-no_presentados = list(map(lambda x: int(x)-1, re.findall("\d+(?=\s.+Non\sPresentado)", pdf)))
+# Encontramos las líneas de los no presentados, porque se escapan al regex de las notas en números
+no_presentados = list(
+    map(lambda x: int(x)-1, re.findall("\d+(?=\s.+Non\sPresentado)", pdf)))
 #list(map(lambda x: nombres.pop(int(x)-1), no_presentados))
-nombres = list(map(lambda x: x[1], filter(lambda tupla: True if tupla[0] not in no_presentados else False, enumerate(nombres))))
-apellidos = list(map(lambda x: x[1], filter(lambda tupla: True if tupla[0] not in no_presentados else False, enumerate(apellidos))))
+nombres = list(map(lambda x: x[1], filter(
+    lambda tupla: True if tupla[0] not in no_presentados else False, enumerate(nombres))))
+apellidos = list(map(lambda x: x[1], filter(
+    lambda tupla: True if tupla[0] not in no_presentados else False, enumerate(apellidos))))
 #[nombres[index_nombre] for index_nombre in range(nombres) if index_nombre not in no_presentados]
 
-with open(nombre_arch.replace(".pdf",".csv") if ".pdf" in nombre_arch else nombre_arch + ".csv", mode='w', newline='') as arch_csv:
+with open(nombre_arch.replace(".pdf", ".csv") if ".pdf" in nombre_arch else nombre_arch + ".csv", mode='w', newline='') as arch_csv:
     csv_writer = csv.writer(arch_csv, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-    csv_writer.writerow(["Apellidos","Nombre","Nota"])
-    csv_writer.writerows(zip(apellidos,nombres,notas))
+    csv_writer.writerow(["Apellidos", "Nombre", "Nota"])
+    csv_writer.writerows(zip(apellidos, nombres, notas))
 
-#Calculamos parámetros estadísticos
-nombres_parametros = ["media","mediana"]
-valores_parametros = [statistics.mean(notas),statistics.median(notas)]
+# Calculamos parámetros estadísticos
+nombres_parametros = ["media", "mediana"]
+valores_parametros = [statistics.mean(notas), statistics.median(notas)]
 parametros = zip(nombres_parametros, valores_parametros)
 print("\nPARAMETROS\n" + "\n".join(list(map(lambda x: ": ".join((x[0], str(x[1]))), parametros))))
---
+++ readme.md
@@ -0,0 +1,10 @@
+# usc-grades-parser
+## Script de Python 3 para obtener automáticamente las estadísticas de PDFs de la USC
+Este repositorio incluye un script que, partiendo de un documento PDF de cualificaciones (provisionales o definitivas) de la Universidad de Santiago de Compostela, cuyo nombre se indica en tiempo de ejecución, recupera automáticamente los datos del mismo y ofrece estadísticas sobre ellos.
+
+---
+
+Dependencias:
+
+- Popper
+- pdftotext
